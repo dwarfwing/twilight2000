1af61c8ffccbcca01fe709c0a6300a0d99ef7ba8
dwarfwing <65122688+dwarfwing@users.noreply.github.com>
2021-07-20 22:25:47 +0200

    Added tabs, image borders, background image

--- Twilight 2000/source/app/workers/scripts/worker.js
+++ Twilight 2000/source/app/workers/scripts/worker.js
@@ -1,6 +1,16 @@
 /**
   Example worker function
-*/
+
 on('change:', function() {
   setAttrs({});
+});
+*/
+
+const buttonlist = ["basic","attributes","weapons","combat","gear","vehicle","notes"];
+buttonlist.forEach(button => {
+    on(`clicked:${button}`, function() {
+        setAttrs({
+            sheetTab: button
+        });
+    });
 });
